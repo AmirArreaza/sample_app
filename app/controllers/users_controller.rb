ef49fc9331035c16c985d3dacf91d925a2433dee
AmirArreaza <ajaarreazav@gmail.com>
2012-08-30 16:07:48 -0430

    Finalizado Capitulo 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -41,19 +42,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
+
+    # User.find(params[:id]).destroy unless current_user?(@user)
+    #
+    #
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_url
   end
 
   private
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
